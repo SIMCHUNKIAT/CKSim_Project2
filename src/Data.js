295479d4b1a56270dc9763c8a2583fb9066394fa
unknown <chunkiat.sim@gmail.com>
2023-05-23 17:29:48 +0800

    first attempt to perform API call, some errors

--- src/Data.js
+++ src/Data.js
@@ -1,10 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Data() {
+  const [airport, setAirport] = useState("");
+  const [airline, setAirline] = useState("");
+  const [date, setDate] = useState("");
+  const [flight, setFlight] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = `https://aviation-edge.com/v2/public/flightsHistory?key=80d26f-16e109&code=${airport}&type=arrival&date_from=${date}&airline_iata=${airline}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      setFlight(data);
+    };
+
+    fetchData();
+  }, [airport, airline, date]);
+
+console.log("Flightlist:", flight);
 
   return (
-    <h1>Data Page</h1>
+    <div>
+      <h1>Data Page</h1>
+      <form>
+        <label>
+          Airport:
+          <input
+            type="text"
+            value={airport}
+            onChange={(e) => setAirport(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Airline:
+          <input
+            type="text"
+            value={airline}
+            onChange={(e) => setAirline(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Date:
+          <input
+            type="text"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </label>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Flight Number</th>
+            <th>Departure Airport</th>
+            <th>Arrival Airport</th>
+          </tr>
+        </thead>
+        <tbody>
+          {flight.map((flight) => (
+            <tr key={flight.number}>
+              <td>{flight.number}</td>
+              <td>{flight.departureAirport}</td>
+              <td>{flight.arrivalAirport}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
-export default Data
+export default Data;
--- src/Description.js
+++
@@ -1,6 +0,0 @@
-export default function Description() {
-    return <div>
-        <p>The purpose of this application is to provide the size of the market based on the actual flight hour data collected from Aviation Edge.</p>
-        <p>There are 3 pages to this application. This is the home page which provides a brief explanation of the application. The Data page alllows the user to list all the flights within the selection criteria made by the user and the Analytics page performs calculation to provide the market size of the group of products selected.</p>
-    </div>
-}
