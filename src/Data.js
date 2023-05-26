c554a9472130853a9a25daa8ef561ad88d795caa
unknown <chunkiat.sim@gmail.com>
2023-05-26 11:16:20 +0800

    resolved API call issue

--- src/Data.js
+++ src/Data.js
@@ -1,75 +1,80 @@
 import React, { useState, useEffect } from "react";
 
 function Data() {
-  const [airport, setAirport] = useState("");
-  const [airline, setAirline] = useState("");
-  const [date, setDate] = useState("");
-  const [flight, setFlight] = useState([]);
+    const [airport, setAirport] = useState("");
+    const [airline, setAirline] = useState("");
+    const [date, setDate] = useState("");
+    const [flights, setFlights] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const url = `https://aviation-edge.com/v2/public/flightsHistory?key=80d26f-16e109&code=${airport}&type=arrival&date_from=${date}&airline_iata=${airline}`;
-      const res = await fetch(url);
-      const data = await res.json();
-      setFlight(data);
-    };
+    const url = `https://aviation-edge.com/v2/public/flightsHistory?key=80d26f-16e109&code=${airport}&type=arrival&date_from=${date}&airline_iata=${airline}`;
 
-    fetchData();
-  }, [airport, airline, date]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await fetch(url);
+            const data = await res.json();
+            setFlights(data);
+            // console.log(data);
+            // console.log(data[0].airline);
+        };
 
-console.log("Flightlist:", flight);
-
-  return (
-    <div>
-      <h1>Data Page</h1>
-      <form>
-        <label>
-          Airport:
-          <input
-            type="text"
-            value={airport}
-            onChange={(e) => setAirport(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Airline:
-          <input
-            type="text"
-            value={airline}
-            onChange={(e) => setAirline(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Date:
-          <input
-            type="text"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-          />
-        </label>
-      </form>
-      <table>
-        <thead>
-          <tr>
-            <th>Flight Number</th>
-            <th>Departure Airport</th>
-            <th>Arrival Airport</th>
-          </tr>
-        </thead>
-        <tbody>
-          {flight.map((flight) => (
-            <tr key={flight.number}>
-              <td>{flight.number}</td>
-              <td>{flight.departureAirport}</td>
-              <td>{flight.arrivalAirport}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+        fetchData();
+    
+    }, [airport, airline, date]);
+console.log(flights);
+    return (
+        <div>
+            <h1>Data Page</h1>
+            <form>
+                <label>
+                    Airport:
+                    <input
+                        type="text"
+                        value={airport}
+                        onChange={(e) => setAirport(e.target.value)}
+                    />
+                </label>
+                <br />
+                <label>
+                    Airline:
+                    <input
+                        type="text"
+                        value={airline}
+                        onChange={(e) => setAirline(e.target.value)}
+                    />
+                </label>
+                <br />
+                <label>
+                    Date:
+                    <input
+                        type="text"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                    />
+                </label>
+            </form> 
+            <table>
+                <thead>
+                    <tr>
+                        <th>Airline</th>
+                        <th>Flight Number</th>
+                        <th>Departure Airport</th>
+                        <th>Departure Time</th>
+                        <th>Arrival Airport</th>
+                        <th>Arrival Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Array.isArray(flights) && flights.map((flight, index) => (
+                        <tr key={index}>
+                            <td>{flight.airline.name}</td>
+                            <td>{flight.flight.number}</td>
+                            <td>{flight.departure.iataCode}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    ); 
 }
 
 export default Data;
